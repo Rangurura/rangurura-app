6d60c55e9aa3792be1b6c291a823ab75ff85bae3
davidnyiringabo <nyiringabodavid62@gmail.com>
2023-12-31 11:29:31 +0200

    fix: fix typo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 "use client"
 import React from 'react';
 import NavBar from '@/components/Unused/navbar.jsx';
-import Work from '@/Components/Unused/work.jsx';
-import Problem from '@/Components/Unused/problem.jsx';
-import Report from '@/Components/Unused/report.jsx';
-import Contact from '@/Components/Unused/contact.jsx';
-import Questions from '@/Components/Unused/questions.jsx';
-import Footer from '@/Components/Unused/footer.jsx';
+import Work from '@/components/Unused/work.jsx';
+import Problem from '@/components/Unused/problem.jsx';
+import Report from '@/components/Unused/report.jsx';
+import Contact from '@/components/Unused/contact.jsx';
+import Questions from '@/components/Unused/questions.jsx';
+import Footer from '@/components/Unused/footer.jsx';
 import Testimonies from '@/components/Unused/Testimonies';
 
 const Homepage=()=>{
