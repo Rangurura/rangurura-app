6657cc79093301555fb7cb13ee3000f1ddc1789a
davidnyiringabo <nyiringabodavid62@gmail.com>
2024-01-04 06:43:03 +0200

    feat: chat page finished

--- src/typings/index.tsx
+++ src/typings/index.tsx
@@ -1,5 +1,16 @@
+import { StaticImageData } from "next/image";
+
 export type Problem = {
   description: string;
   level: string;
   completed: boolean;
 };
+
+export type Chat = {
+  image: StaticImageData,
+  user: string,
+  time: string,
+  latestMessage: string,
+  id: number,
+  status: string
+}
