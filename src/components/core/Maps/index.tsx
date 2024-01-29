6bd46a56e7cf9a85226f706cc2b11bd535bcdda1
davidnyiringabo <nyiringabodavid62@gmail.com>
2024-01-29 13:16:27 +0200

    feat: fix the landing navbar

--- src/components/core/Maps/index.tsx
+++ src/components/core/Maps/index.tsx
@@ -1,12 +1,17 @@
-"use client"
+"use client";
 import { MapContainer } from "react-leaflet/MapContainer";
 import { TileLayer } from "react-leaflet/TileLayer";
 import { useMap } from "react-leaflet/hooks";
 import { Marker, Popup } from "react-leaflet";
-import "leaflet/dist/leaflet.css"
+import "leaflet/dist/leaflet.css";
 const LiveMap = () => {
   return (
-    <MapContainer style={{width:"100%", height:"70%"}} center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+    <MapContainer
+      style={{ width: "100%", height: "70%" }}
+      center={[51.505, -0.09]}
+      zoom={13}
+      scrollWheelZoom={false}
+    >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
