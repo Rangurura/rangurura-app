e17512aff3c28102321f61f9380bc99b06788ecc
davidnyiringabo <nyiringabodavid62@gmail.com>
2024-02-03 18:00:46 +0200

    fix: fix error in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,7 +9,7 @@ export default function middleware(
 ) {
   const token = getCookie("token");
   console.log(token);
-  if (!token) {
+  if (!token && req.url !== "/") {
     return NextResponse.redirect(new URL("/login", req.url));
   }
 
@@ -18,6 +18,6 @@ export default function middleware(
 
 export const config = {
   matcher: [
-    "/((?!api|/|login|register/*|verify|_next/static|public|_next/image|favicon.ico|images|logo.svg|logo.png|rca.jpeg|favicon.svg|favicon.png).*)",
+    "/((?!api|login|register/*|verify|_next/static|public|_next/image|favicon.ico|images|logo.svg|logo.png|rca.jpeg|favicon.svg|favicon.png).*)",
   ],
 };
