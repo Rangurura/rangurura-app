932aa127e7b176040f256d63086e7c080973b8d4
davidnyiringabo <nyiringabodavid62@gmail.com>
2024-02-04 15:56:00 +0200

    feat: change landing page stuffs

--- src/components/Unused/work.jsx
+++ src/components/Unused/work.jsx
@@ -19,7 +19,7 @@ const Work = () => {
         us help you build a better tomorrow with Rangurura
       </p>
       <div className="w-full flex flex-col md:flex-row justify-evenly">
-        <div className="flex flex-col md:gap-24 gap-6 py-4 justify-center items-center mt-3 md:mt-0">
+        <div className="flex flex-col md:gap-24 gap-6 py-4 justify-center items-center mt-3 md:mt-12">
           <div className="flex justify-center items-center">
             <Image src={work} alt="" className="md:w-[30rem] w-[24rem]" />
           </div>
@@ -28,52 +28,46 @@ const Work = () => {
           {/* <div className='max-[400px]:flex-col flex flex-row justify-center items-start md:gap-16 gap-6 md:px-16 px-0'> */}
           {/* <div className="flex flex-col md:mt-32 gap-6 md:pt-[5rem]"> */}
           {/* <div className='flex flex-col md:gap-12 md:mt-32 gap-6'> */}
-          <div className="flex flex-col md:gap-4 gap-1 justify-center items-center">
-            <Image src={flash} alt="" className="md:w-8 w-8 " />
+          <div className="flex flex-col md:gap-4 gap-1 justify-center items-start">
+            {/* <Image src={flash} alt="" className="md:w-8 w-8 " /> */}
             <h4 className="font-bold md:text-md text-sm">Faster</h4>
             <p className="md:text-base text-xs">
-              Problem reach to the destination safe and faster through our
-              platform
+              Problem reach to the destination safe and faster
             </p>
           </div>
-          <div className="flex flex-col gap-4 justify-center items-center md:mt-[3.4rem]">
-            <Image src={privacy} alt="" className="md:w-8 w-8 " />
+          <div className="flex flex-col gap-4 justify-center items-start">
+            {/* <Image src={privacy} alt="" className="md:w-8 w-8 " /> */}
             <h4 className="font-bold md:text-md text-sm">Privacy</h4>
             <p className="md:text-base text-xs">
-              We ensure privacy by hiding the person who report his or her
-              concern to the one who was tagged .
+            We ensure privacy by hiding the reporter’s information
             </p>
           </div>
           {/* </div> */}
           {/* <div className="flex flex-col md:gap-12 md:mt-20 gap-6 md:pt-[8rem]"> */}
-          <div className="flex flex-col md:gap-4 gap-1 justify-center items-center">
-            <Image src={message} alt="" className="md:w-9 w-5" />
+          <div className="flex flex-col md:gap-4 gap-1 justify-center items-start">
+            {/* <Image src={message} alt="" className="md:w-9 w-5" /> */}
             <h4 className="font-bold md:text-md text-sm">Report problems</h4>
             <p className="md:text-base text-xs">
-              Whether it's a pothole, a public service issue, or a community
-              concern, you can easily report it to your local government through
-              our platform.
+              you can easily report it to your local government through our platform.
             </p>
           </div>
-          <div className="flex flex-col md:gap-4 gap-1 justify-center items-center">
-            <Image src={track} alt="" className="md:w-9 w-5" />
+          <div className="flex flex-col md:gap-4 gap-1 justify-center items-start">
+            {/* <Image src={track} alt="" className="md:w-9 w-5" /> */}
             <h4 className="font-bold md:text-md text-sm">
               Transparent process
             </h4>
             <p className="md:text-base text-xs">
-              We ensure transparency by tracking the progress of your reports,
-              so you can see when and how your issues are being addressed.
+              We ensure transparency by tracking the progress of your reports.
             </p>
           </div>
           {/* </div> */}
-          <div className="flex flex-col justify-center items-center gap-4 mt-5">
-            <Image src={people} alt="" className="md:w-8 w-8 " />
+          <div className="flex flex-col justify-center items-start gap-4">
+            {/* <Image src={people} alt="" className="md:w-8 w-8 " /> */}
             <h4 className="font-bold md:text-md text-sm">
               Community Engagement
             </h4>
-            <p className="w-full md:text-base text-sm">
-              Join hands with fellow citizens by reporting your concern to
-              create a stronger community to solve local issues together.
+            <p className="w-full md:text-base text-xs">
+              Join hands with fellow citizens by reporting your concern 
             </p>
           </div>
         </div>
