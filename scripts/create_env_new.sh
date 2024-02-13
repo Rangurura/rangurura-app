6fc66122dab4f98e5ffa536c8bb330fe6234819d
VanY <apple@APPLEs-MacBook-Pro.local>
2024-02-13 21:31:59 +0200

    [fix]: dep.yml

--- scripts/create_env_new.sh
+++ scripts/create_env_new.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Specify the content to be written to the .env.local file
-ENV_CONTENT="# TEST\nVITE_API_URL=http://194.163.167.131:9088/rhip/api/v1\nVITE_BASE_URL=http://194.163.167.131:9088/rhip"
+ENV_CONTENT="# TEST\nVITE_API_URL=http://194.163.167.131:9000/rangurura/api/v1\nVITE_BASE_URL=http://194.163.167.131:9000/rangurura"
 
 # Check if .env.local file exists
 if [ -f .env.local ]; then
