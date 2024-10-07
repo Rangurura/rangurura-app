ff1c1b7a0cbbb42404e12f6a9dc37c8711e23176
David NYIRINGABO <nyiringabodavid62@gmail.com>
2024-10-07 20:36:58 +0200

    fix: fix vercel middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
 import { NextResponse, type NextRequest } from "next/server";
 
 export function middleware() {
-  NextResponse.redirect(new URL("http://10.10.77.40:7899"))
+  return NextResponse.redirect(new URL("http://10.10.77.40:7899"))
 }
