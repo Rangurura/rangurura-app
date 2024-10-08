1141420267bc053fda497ab07b04d95a30042460
David NYIRINGABO <nyiringabodavid62@gmail.com>
2024-10-08 09:01:27 +0200

    fix: change the redirect URL

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
 import { NextResponse, type NextRequest } from "next/server";
 
 export function middleware() {
-  return NextResponse.redirect(new URL("http://10.10.77.40:7899"))
+  return NextResponse.redirect(new URL("http://197.243.23.139:7899"))
 }
